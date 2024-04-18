1a4bbe6ffaa0a2c1b2216a354ecfbdcb8d94d2a2
Robin Ellingsen <robin-elli@hotmail.com>
2024-04-18 11:52:20 +0200

    matrikkel return either units from unopened envelope or open envelope

--- matrikkelenheter/index.js
+++ matrikkelenheter/index.js
@@ -30,10 +30,17 @@ module.exports = async function (context, req) {
   const client = new MatrikkelClient(matrikkelApi.MATRIKKELAPI_USERNAME, matrikkelApi.MATRIKKELAPI_PASSWORD, 'matrikkelapi/wsapi/v1/MatrikkelenhetServiceWS')
 
   const result = await client.getMatrikkelPolygon(req, req.body.polygon)
+
+  let units
+  if(result[0]['soap:Envelope']?.['soap:Body'].findMatrikkelenheterResponse.return !== undefined) {
+    units = result[0]['soap:Envelope']?.['soap:Body'].findMatrikkelenheterResponse.return.item.map(unit => unit.value)
+  } else{
+    units = result[0].findMatrikkelenheterResponse?.return?.item.map(unit => unit.value)
+  }
+
+  // const units = result[0].findMatrikkelenheterResponse?.return?.item.map(unit => unit.value)
   
-  const units = result[0].findMatrikkelenheterResponse.return.item.map(unit => unit.value)
-  
-  if(units.length < 0) throw new Error('Fant ingen enheter innenfor polygonet')
+  if(units === undefined || units.length < 0) throw new Error('Fant ingen enheter innenfor polygonet')
 
   try {
     return { status: 200, body: { units, koordinatsystemKodeId: req.body.koordinatsystemKodeId } }
